884c6df93058a8f234550ce31d16ccb37d40edd0
Johan Kämpe <kampe.johan@gmail.com>
2017-10-25 09:22:21 +0200

    Use own functions to set/read time

--- code/testing/RTC/RTC.ino
+++ code/testing/RTC/RTC.ino
@@ -1,45 +1,58 @@
 #include <Wire.h>
 #include <RTC_DS3231.h>
 
+#define DS3231_I2C_ADDRESS 0x68
+
 /*
  * Koppling
  * PB6 - SCL
  * PB7 - SDA
- * PA0 - SQW
  */
-
-#define LED PC13
-#define SQW PA0
 
 DS3231 rtc;
 
-void handler();
-volatile int state = LOW;
-
 void setup()
 {
-  Serial.begin(9600);
-  Wire.begin();
+    Serial.begin(9600);
+    Wire.begin();
 
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, state);
-  pinMode(SQW, INPUT_PULLUP);
+    set_date_time();
 
-  // RTC Setup ////////
-  rtc.force_update(); // get date from RTC
-  rtc.set_self_update(ENABLE); // enable calculation date on  microcontroller side
-  rtc.set_sqw(DS3231_1HZ); // enable calculation date through external interrupt, 1sec=1tick
-  rtc.reset_control(DS3231_INTCH); // enable SQW mode for INT/SQW pin DS3231
 }
 
 void loop()
 {
-  rtc.print_calendar();
-  state=!state;
-  digitalWrite(LED, state);
-  delay(1000);
+    rtc.print_calendar();
+    delay(1000);
 }
 
-void handler() {
-  rtc++;
+/* Fix dec to bcd coding */
+void set_date_time()
+{
+    Wire.beginTransmission(DS3231_I2C_ADDRESS);
+    Wire.write(0x00);
+    Wire.write(30);  // Second
+    Wire.write(10);  // Minute
+    Wire.write(10);  // Hour
+    Wire.write(3);   // Day 1-7
+    Wire.write(20);  // Date 1-31
+    Wire.write(12);  // Month 1-12 + Century
+    Wire.write(17);  // Year 0-99
+    Wire.endTransmission();
+}
+
+/* Fix bcd to dec coding */
+void get_date_time()
+{
+    Wire.requestFrom(DS3231_I2C_ADDRESS, 0x00);
+    Wire.beginTransmission(DS3231_I2C_ADDRESS);
+    Wire.write(0x00);
+    Wire.write(30);  // Second
+    Wire.write(10);  // Minute
+    Wire.write(10);  // Hour
+    Wire.write(3);   // Day 1-7
+    Wire.write(20);  // Date 1-31
+    Wire.write(12);  // Month 1-12 + Century
+    Wire.write(17);  // Year 0-99
+    Wire.endTransmission();
 }
