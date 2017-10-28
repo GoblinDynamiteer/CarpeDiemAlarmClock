97d03e28f39cae31f30fdc46e9c3f73441f73370
Johan Kämpe <kampe.johan@gmail.com>
2017-10-28 20:25:57 +0200

    Added option to pass bin-file as parameter

--- stlink_program.bat
+++ stlink_program.bat
@@ -1,6 +1,7 @@
 SET stlink_path="C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe"
-SET bin_path=%~dp0code\build\CarpeDiemAlarmClock.ino.bin
 SET address=0x08000000
 SET verify_mode="while_programming"
 SET settings=HOTPLUG
+IF NOT DEFINED %~1 (SET bin_path=%~dp0code\build\CarpeDiemAlarmClock.ino.bin)
+IF DEFINED %~1 (SET bin_path=%~1)
 %stlink_path% -c %settings% -P %bin_path% %address% -V %verify_mode% -Rst -Run
