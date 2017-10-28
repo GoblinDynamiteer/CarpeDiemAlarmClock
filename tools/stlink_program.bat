4da521d6bd82d12fa259bdbc526f181b0d54b7ed
Johan Kämpe <kampe.johan@gmail.com>
2017-10-28 13:21:28 +0200

    Program / reset with STL-Link CLI. Pass path to BIN-file as arg to bat-file

---
+++ tools/stlink_program.bat
@@ -0,0 +1,6 @@
+SET stlink_path="C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe"
+SET bin_path=%1
+SET address=0x08000000
+SET verify_mode="while_programming"
+SET settings=HOTPLUG
+%stlink_path% -c %settings% -P %bin_path% %address% -V %verify_mode% -Rst -Run
